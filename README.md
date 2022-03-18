c6ef5caabec9065e775629ea8f8ea506b07b2d01
Trung Nguyen <trungnguyen@Trungs-MBP.ht.home>
2022-03-18 13:34:25 -0400

    structure

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Quiz-Code-Challenege
+Review the mock up I will divide the task as below:
+
+* Quiz
+    * * Make an input box asking for the name
+    * * I will create a questions and answers pool (array)
+    * * Questions will display on the main screen randomly (mathfloor, math random)
+    * * when I mouseover the answers, it will appear special style (hover or mouse over)
+    * * when I click the answer, the answer keep special style, then I create a button to submit the answer, the result will show if it's corect or wrong (if: boolean)
+    * * After show the result, a score will be counted or deducted in the local storage depends on the answer and shows after finishing the quizzes
+    * * All the final score will be saved and stored in the " View High scores" section, whatever is higher will be shown on top with number ranking ()
+    * * set a Timer for the quiz process (setInterval, clearInterval)
+    
+
+    
---
+++ assets/js/quiz.js
@@ -0,0 +1,82 @@
+var quizzQuestion=[{
+    q: "What is CSS stand for?",
+    a:['Cascading Style Sheets','Creative Style Sheets','Colorful Style Sheets','Computer Style Sheets'],
+    correct:'a'
+},
+
+{
+    q:"What does the following mean to the computer? div p {color: #ff0000;}",
+    a:[
+    'Make text in all divs and all paragraphs in this document be bright red',
+    'Make the background in all divs that are of the class "p" be bright red',
+    'Make the text inside any divs inside of any paragraph be bright red',
+    'Make the text inside any paragraph that is inside any div be bright red'],
+    correct:'b'
+},
+
+{
+    q: "Which HTML attribute is used to define inline styles?",
+    a:['font','styles','style','class'],
+    correct:'b'
+
+},
+
+{
+    q:"Which is the correct CSS syntax?",
+    a:['{body:color=black;}','body{color:black;}','body:color=black;','{body;color:black;}'],
+    correct:'b'
+},
+
+{
+    q:"How do you insert a comment in a CSS file?",
+    a:['// this is a comment','/* this is a comment*/','.this is a comment','//this is a comment//'],
+    correct:'b'
+}];
+
+
+var currentQuestion= 0;
+const startBtn=document.getElementById("start-button");
+const TiMer= document.getElementById("TiMer");
+const q=document.getElementById("questions");
+const a= document.getElementById("answer-buttons");
+const submitBtn=document.getElementById("submit-button");
+const scoRe= document.getElementById("score");
+var correct
+ 
+
+startBtn.addEventListener('click',showQuestion);
+submitBtn.addEventListener('click',currentQuestion)
+
+function showQuestion(){
+var tiTle= document.querySelector(".title");
+var conTent=document.querySelector(".content");
+
+
+}
+
+function showAnswer(){
+
+
+}
+
+function currentQuestion(){
+
+
+}
+
+function scoreRender(){
+
+
+
+}
+
+function timeLeft(){
+
+
+
+}
+
+
+
+
+
